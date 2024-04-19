2b3268ac039040d30a5f7b22003c337ea351abf3
Vladimir Mikheev <dezem@bk.ru>
2024-04-19 11:41:33 +0000

    indexing solved

--- src/interactive.jl
+++ src/interactive.jl
@@ -80,55 +80,67 @@ function x1(evts, image)
     
     indices_notnan = @lift(findall(<(1), isnan.(evts[:, $var_i[]])))
     data_view = @lift(@view(erps[$chan_i, :, $indices_notnan]))
-    str = @lift("ERP image: channel " * string($chan_i) * ", variable" * string($var_i))
+    str = @lift("ERP image: channel " * string($chan_i) * ", variable " * string($var_i))
     plot_erpimage!(
         f[2, 1:5],
         data_view;
         sortvalues = @lift(evts[$indices_notnan, $var_i[]]),
         axis = (; title = str),
     )
-    println(size(m))
-    on(events(f).mousebutton, priority = 2) do event
+    on(events(f).mousebutton, priority = 1) do event
         if event.button == Mouse.left && event.action == Mouse.press
-            _, i = pick(hm)
-            chan_i[] = mod(i[], size(m, 1))
-            var_i[] = Integer(floor(i[] / size(m, 1)))
-            println(i[], ' ', chan_i[], ' ', var_i[])
+            plot, _ = pick(ax.scene)
+            a = DataInspector(plot)
+            pos = Makie.position_on_plot(plot, -1, apply_transform = false)[Vec(1, 2)]
+            b = Makie._pixelated_getindex(plot[1][], plot[2][], plot[3][], pos, true)
+            chan_i[], var_i[] = b[1], b[2]
+            #println(chan_i[], ' ', var_i[])
         end
     end
     f
 end
+x1(evts[1:5, 2:end], image[1:5, 2:end])
+
 x1(evts[1:50, 2:end], image[1:50, 2:end])
 
 x1(evts[:, 2:end], image[:, 2:end])
 
-function y()
+function y(image)
     var_i = Observable(1)
     chan_i = Observable(1)
-    m = Matrix(image[:, 1:6])
-    f = Figure()
-    ax = WGLMakie.Axis(f[1, 1:4], title = "Entropy d image", xlabel = "Channels", ylabel = "Index of event variable")
+    m = Matrix(image)
+    f = Figure(size = (600, 600))
+    str = @lift("Entropy d image, indexes: " * string($chan_i) * ", " * string($var_i))
+ 
+    ax = WGLMakie.Axis(f[1, 1:4], 
+        xautolimitmargin = (0, 0),
+        yautolimitmargin = (0, 0),
+        title = str, xlabel = "Channels", ylabel = "Index of event variable"
+    )
+    ax.yticks = 1:size(m, 2)
+    ax.xticks = 1:size(m, 1) 
     hm = heatmap!(ax, m)
     Colorbar(f[1, 5], hm, labelrotation = -π / 2, label = "Entropy d")
 
-    str = lift((var_i, chan_i) -> "Entropy d image, indexes: $(var_i), $(chan_i)", var_i, chan_i)
-    on(str) do string
-        ax.title = string
-    end
-    on(events(f).mousebutton, priority = 2) do event
+    on(events(f).mousebutton, priority = 1) do event
         if event.button == Mouse.left && event.action == Mouse.press
-            _, i = pick(ax.scene)
-            chan_i[] = mod(i, size(m, 1))
-            var_i[] = Integer(floor(i / size(m, 1)))
-            println(chan_i[], ' ', var_i[])
+            plot, _ = pick(ax.scene)
+            a = DataInspector(plot)
+            pos = Makie.position_on_plot(plot, -1, apply_transform = false)[Vec(1, 2)]
+            b = Makie._pixelated_getindex(plot[1][], plot[2][], plot[3][], pos, true)
+            chan_i[], var_i[] = b[1], b[2]
+            #println(chan_i[], ' ', var_i[])
         end
     end
     f
 end
-y()
+y(image[1:10, 2:10])
+
+
 
 # to do
 # 1. disable dragging
+
 ###############
 
 
