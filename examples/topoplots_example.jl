5d2f1ea485fe90205c143117587855c005d6d4ec
Vladimir Mikheev <dezem@bk.ru>
2024-08-02 16:28:57 +0000

    now with tests

--- examples/topoplots_example.jl
+++ examples/topoplots_example.jl
@@ -2,7 +2,7 @@ includet("setup.jl")
 includet("../src/topoplots.jl")
 
 Makie.inline!(false)
-using GLMakie
+
 # data
 begin
     fid = h5open("data/data_fixations.hdf5", "r")
@@ -17,45 +17,55 @@ begin
 end
 
 begin
-    tmp = stack(evts_d)
-    tmp.timing = 1:nrow(tmp)
-    tmp.label = 1:nrow(tmp)
-    rename!(tmp, :variable => :condition, :value => :estimate)
-    tmp.rows = vcat(
-        repeat(["A"], size(tmp, 1) ÷ 4),
-        repeat(["B"], size(tmp, 1) ÷ 4),
-        repeat(["C"], size(tmp, 1) ÷ 4),
-        repeat(["D"], size(tmp, 1) ÷ 4),
+    pattern_detection_values = stack(evts_d)
+    pattern_detection_values.timing = 1:nrow(pattern_detection_values)
+    pattern_detection_values.label = 1:nrow(pattern_detection_values)
+    rename!(pattern_detection_values, :variable => :condition, :value => :estimate)
+    pattern_detection_values.rows = vcat(
+        repeat(["A"], size(pattern_detection_values, 1) ÷ 4),
+        repeat(["B"], size(pattern_detection_values, 1) ÷ 4),
+        repeat(["C"], size(pattern_detection_values, 1) ÷ 4),
+        repeat(["D"], size(pattern_detection_values, 1) ÷ 4),
     )
 end
 
-inter_topo(filter(x -> x.rows == "A", tmp))
-inter_topo(filter(x -> x.rows == "B", tmp))
-inter_topo(filter(x -> x.rows == "C", tmp))
-inter_topo(filter(x -> x.rows == "D", tmp))
+inter_topo(filter(x -> x.rows == "A", pattern_detection_values))
+inter_topo(filter(x -> x.rows == "B", pattern_detection_values))
+inter_topo(filter(x -> x.rows == "C", pattern_detection_values))
+inter_topo(filter(x -> x.rows == "D", pattern_detection_values))
 
-inter_topo_image(tmp1, evts, erps)
+inter_topo_image(pattern_detection_values1, evts, erps)
 
-inter_topo_image(filter(x -> x.rows == "A", tmp), evts, erps_fix, timing)
-inter_topo_image(filter(x -> x.rows == "B", tmp), evts, erps_fix, timing) #fails
-inter_topo_image(filter(x -> x.rows == "C", tmp), evts, erps_fix, timing)
-inter_topo_image(filter(x -> x.rows == "D", tmp), evts, erps_fix, timing)
+inter_topo_image(filter(x -> x.rows == "A", pattern_detection_values), evts, erps_fix, timing)
+inter_topo_image(filter(x -> x.rows == "B", pattern_detection_values), evts, erps_fix, timing) #fails
+inter_topo_image(filter(x -> x.rows == "C", pattern_detection_values), evts, erps_fix, timing)
+inter_topo_image(filter(x -> x.rows == "D", pattern_detection_values), evts, erps_fix, timing)
 
-inter_topo_image(filter(x -> x.condition == "fix_samebox", tmp), evts, erps_fix, timing)
+inter_topo_image(filter(x -> x.condition == "fix_samebox", pattern_detection_values), evts, erps_fix, timing)
+
+
+# make that workable
+begin
+    data_all, evts_sim = simulate_alldata()
+    timing = -0.5:0.001953125:1.0
+end
+
+inter_topo_image(pattern_detection_values, evts_sim, data_all, timing; positions_128 = positions_128)
+
 
 # debugging zone
 
 begin
-    tmp2 = stack(evts_mf)
-    tmp2.timing = 1:nrow(tmp)
-    tmp2.label = 1:nrow(tmp)
-    rename!(tmp2, :variable => :condition, :value => :estimate)
-    tmp2.rows = vcat(
-        repeat(["A"], size(tmp2, 1) ÷ 4),
-        repeat(["B"], size(tmp2, 1) ÷ 4),
-        repeat(["C"], size(tmp2, 1) ÷ 4),
-        repeat(["D"], size(tmp2, 1) ÷ 4),
+    pattern_detection_values2 = stack(evts_mf)
+    pattern_detection_values2.timing = 1:nrow(pattern_detection_values)
+    pattern_detection_values2.label = 1:nrow(pattern_detection_values)
+    rename!(pattern_detection_values2, :variable => :condition, :value => :estimate)
+    pattern_detection_values2.rows = vcat(
+        repeat(["A"], size(pattern_detection_values2, 1) ÷ 4),
+        repeat(["B"], size(pattern_detection_values2, 1) ÷ 4),
+        repeat(["C"], size(pattern_detection_values2, 1) ÷ 4),
+        repeat(["D"], size(pattern_detection_values2, 1) ÷ 4),
     )
 end
 
-inter_topo_image(filter(x -> x.rows == "A", tmp2), evts, erps_fix, timing)
+inter_topo_image(filter(x -> x.rows == "A", pattern_detection_values2), evts, erps_fix, timing)
