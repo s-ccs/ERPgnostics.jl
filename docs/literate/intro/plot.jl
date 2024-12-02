7bf252edb64f5b837c1a20e4813d235e716c8861
Vladimir Mikheev <dezem@bk.ru>
2024-12-02 13:36:23 +0100

    bug 6

--- docs/literate/intro/plot.jl
+++ docs/literate/intro/plot.jl
@@ -3,9 +3,10 @@ using DataFrames
 using UnfoldMakie
 using JLD2
 
+# Data input
 include("../../../example_data.jl")
 
-positions_128 = JLD2.load_object("../../../data/positions_128.jld2")
+positions_128 = JLD2.load_object("../../../../data/positions_128.jld2")
 pattern_detection_values = example_data()
 
 # Example of interactive topoplot series
