0775cacfda92cbd497df59bfdee4733d07cc4b3e
Vladimir Mikheev <dezem@bk.ru>
2024-12-02 12:17:11 +0100

    bug 5

--- docs/literate/intro/plot.jl
+++ docs/literate/intro/plot.jl
@@ -5,7 +5,7 @@ using JLD2
 
 include("../../../example_data.jl")
 
-positions_128 = JLD2.load_object("data/positions_128.jld2")
+positions_128 = JLD2.load_object("../../../data/positions_128.jld2")
 pattern_detection_values = example_data()
 
 # Example of interactive topoplot series
