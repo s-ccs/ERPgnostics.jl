40b15cc09f7ba5c8ea593713b345f96b834dd91b
Vladimir Mikheev <dezem@bk.ru>
2024-12-04 15:36:26 +0100

    bug 3

--- docs/example_data.jl
+++ docs/example_data.jl
@@ -17,14 +17,20 @@ Datasets:
 **Return Value:** `DataFrame`.
 """
 function example_data(example = "pattern_detection_values"; mode = 1)
+    if mode == 1
+        datapath = dirname(dirname(Base.current_project())) * "/data/evts_d.csv"
+    else
+        datapath = dirname(Base.current_project()) * "/data/evts_d.csv"
+    end
     if example == "pattern_detection_values"
-        if mode == 1
-            datapath = dirname(dirname(Base.current_project())) * "/data/evts_d.csv"
-        else
-            datapath = dirname(Base.current_project()) * "/data/evts_d.csv"
-        end
         evts_d = CSV.read(datapath, DataFrame)
         pattern_detection_values = stack(evts_d)
+        rename!(pattern_detection_values, :variable => :condition, :value => :estimate)
+        evts_d = nothing
+        return pattern_detection_values
+    elseif example == "pattern_detection_values_32"
+        evts_d = CSV.read(datapath, DataFrame)
+        pattern_detection_values = stack(evts_d[1:32, :])
         rename!(pattern_detection_values, :variable => :condition, :value => :estimate)
         evts_d = nothing
         return pattern_detection_values
