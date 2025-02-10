fe61ff4b5fbcbc5a9dc7c50124a62757268b577a
Vladimir Mikheev <dezem@bk.ru>
2025-02-10 14:55:41 +0100

    format

--- test/interactive_heatmap_test.jl
+++ test/interactive_heatmap_test.jl
@@ -3,7 +3,7 @@ begin
     pattern_detection_values = CSV.read("../data/evts_d.csv", DataFrame)
 end
 
-@testset "inter_heatmap" begin
+@testset "interactive heatmap" begin
     inter_heatmap(pattern_detection_values)
 end
 
@@ -12,7 +12,7 @@ end
 erps_fix = read(fid["data"]["data_fixations.hdf5"])
 close(fid)
 
-@testset "inter_heatmap_image" begin
+@testset "interactive heatmap image" begin
     inter_heatmap_image(pattern_detection_values, events, erps_fix)
 end
- =#
+=#
