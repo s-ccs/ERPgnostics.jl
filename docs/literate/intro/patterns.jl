bbe55fe96d2fb8a6a7f03a5f6b4e6efc33695d66
Vladimir Mikheev <33777074+vladdez@users.noreply.github.com>
2025-05-20 16:54:04 +0200

    collapsing the code and two column figure

--- docs/literate/intro/patterns.jl
+++ docs/literate/intro/patterns.jl
@@ -7,44 +7,55 @@ using CSV
 data_all, evts = simulate_6patterns()
 
 # Here, you can see how sorting the same data by different values creates six distinct patterns.
-
-let
-    f = Figure(size = (500, 1200))
+# ```@raw html
+# <details>
+# <summary>Click to expand</summary>
+# ```
+function draw_erpimages()
+    f = Figure(size = (900, 600))
     plot_erpimage!(
         f[1, 1],
         data_all;
         sortvalues = evts.Δlatency,
         axis = (; title = "Sigmoid; sorted by Δlatency", xlabelvisible = false, xticklabelsvisible = false),
+        colorbar = (; label = ""),
     )
     plot_erpimage!(
-        f[end+1, 1],
+        f[2, 1],
         data_all;
         sortvalues = evts.duration,
         axis = (; title = "One-sided fan; sorted by duration", xlabelvisible = false, xticklabelsvisible = false),
+        colorbar = (; label = ""),
     )
     plot_erpimage!(
-        f[end+1, 1],
+        f[3, 1],
         data_all;
         sortvalues = evts.durationB,
-        axis = (; title = "Two-sided fan; sorted by durationB", xlabelvisible = false, xticklabelsvisible = false),
+        axis = (; title = "Two-sided fan; sorted by durationB", xlabel = "Time [ms]"),
+        colorbar = (; label = ""),
     )
     plot_erpimage!(
-        f[end+1, 1],
+        f[1, 2],
         data_all;
         sortvalues = evts.condition .== "car",
-        axis = (; title = "Diverging bar; sorted by iscar", xlabelvisible = false, xticklabelsvisible = false),
+        axis = (; title = "Diverging bar; sorted by iscar", xlabelvisible = false, ylabelvisible = false, xticklabelsvisible = false,  yticklabelsvisible = false),
     )
     plot_erpimage!(
-        f[end+1, 1],
+        f[2, 2],
         data_all;
         sortvalues = evts.continuous,
-        axis = (; title = "Hourglass bar; sorted by continuous", xlabelvisible = false, xticklabelsvisible = false),
+        axis = (; title = "Hourglass bar; sorted by continuous", xlabelvisible = false, ylabelvisible = false,  xticklabelsvisible = false,  yticklabelsvisible = false),
     )
     plot_erpimage!(
-        f[end+1, 1],
+        f[3, 2],
         data_all;
         sortvalues = evts.duration_linear,
-        axis = (; title = "Tilted bar; sorted by duration_linear"),
+        axis = (; title = "Tilted bar; sorted by duration_linear", xlabel = "Time [ms]", ylabelvisible = false,  yticklabelsvisible = false),
     )
     f
 end
+# ```@raw html
+# </details >
+#
+```
+draw_erpimages()
